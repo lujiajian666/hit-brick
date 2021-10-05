4572dd176e8a64c4366ce0e507fda886d656dc2e
lujiajian <18928728949@163.com>
2021-10-05 17:08:54 +0800

    feat: 发球，删除球

--- src/page/Index/index.js
+++ src/page/Index/index.js
@@ -4,42 +4,46 @@ import {
   startDrawRect
 } from '../../util/draw'
 import './index.css'
-
+let id = 0
+function createSingleCircle ({ rectPoint, drawList }) {
+  const singleCircle = {
+    id: ++id,
+    handle: startDrawCircle,
+    param: {
+      point: {
+        x: 0,
+        y: 0,
+        r: 5,
+        xDirect: 1,
+        yDirect: 1
+      },
+      rectPoint: rectPoint
+    }
+  }
+  drawList.push(singleCircle)
+}
 function IndexPage () {
   const height = 700
   const width = 500
+  const rectPoint = {
+    x: 200,
+    y: 600,
+    height: 10,
+    width: 120,
+    xDirect: 1,
+    stepLength: 0
+  }
+  const drawList = [
+    {
+      handle: startDrawRect,
+      param: {
+        point: rectPoint
+      }
+    }
+  ]
   setTimeout(() => {
     const context = document.getElementById('canvas').getContext('2d')
     const drawAll = collectDraw(context, width, height)
-    const rectPoint = {
-      x: 200,
-      y: 600,
-      height: 10,
-      width: 120,
-      xDirect: 1,
-      stepLength: 0
-    }
-    drawAll([
-      {
-        handle: startDrawCircle,
-        param: {
-          point: {
-            x: 0,
-            y: 0,
-            r: 5,
-            xDirect: 1,
-            yDirect: 1
-          },
-          rectPoint: rectPoint
-        }
-      },
-      {
-        handle: startDrawRect,
-        param: {
-          point: rectPoint
-        }
-      }
-    ])
 
     document.onkeydown = function (event) {
       const e = event || window.event
@@ -51,11 +55,14 @@ function IndexPage () {
         rectPoint.stepLength = 14
       }
     }
+
+    drawAll(drawList)
   }, 0)
 
   const inner = (
     <div>
       <canvas id="canvas" height={height} width={width} className="canvas"></canvas>
+      <button onClick={() => createSingleCircle({ rectPoint, drawList })}>发球</button>
     </div>
   )
   return inner
--- src/util/draw.js
+++ src/util/draw.js
@@ -1,24 +1,41 @@
+
+function remove (drawList, id) {
+  const index = drawList.findIndex((item) => item.id === id)
+  drawList.splice(index, 1)
+}
 function collectDraw (ctx, width, height) {
   return function drawAll (drawList) {
     ctx.clearRect(0, 0, width, height)
-    drawList.forEach(({ handle, param }) => handle({ ...param, width, height, ctx }))
+    drawList.forEach(({ handle, param, id }) => handle({
+      ...param,
+      width,
+      height,
+      ctx,
+      remove: remove.bind(null, drawList, id)
+    }))
     window.requestIdleCallback(drawAll.bind(null, drawList))
   }
 }
-function startDrawCircle ({ ctx, height, width, point, rectPoint }) {
+function startDrawCircle ({ ctx, height, width, point, rectPoint, remove }) {
   const stepLength = 5
   ctx.beginPath()
   ctx.fillStyle = 'white'
+
+  const targetY = point.y + point.yDirect * stepLength
+  if (targetY > height) {
+    remove()
+    return
+  }
+  if (targetY < 0 || intersect(rectPoint, point)) {
+    point.yDirect *= -1
+  }
+  point.y = point.y + point.yDirect * stepLength
+
   const targetX = point.x + point.xDirect * stepLength
   if (targetX > width || targetX < 0 || intersect(rectPoint, point)) {
     point.xDirect *= -1
   }
   point.x = point.x + point.xDirect * stepLength
-  const targetY = point.y + point.yDirect * stepLength
-  if (targetY > height || targetY < 0 || intersect(rectPoint, point)) {
-    point.yDirect *= -1
-  }
-  point.y = point.y + point.yDirect * stepLength
 
   ctx.arc(point.x, point.y, point.r, 0, 2 * Math.PI)
   ctx.fill()
@@ -39,8 +56,16 @@ function startDrawRect ({ ctx, width, point }) {
   ctx.fill()
 }
 
+// 不能卡在中间，最上边以下的，不算触碰
 function intersect (rect, circle) {
   const targetPoint = {}
+
+  if (circle.y < rect.y + circle.r) {
+    targetPoint.y = rect.y
+  } else {
+    return false
+  }
+
   if (circle.x > rect.x + rect.width) {
     targetPoint.x = rect.x + rect.width
   } else if (circle.x < rect.x) {
@@ -48,15 +73,6 @@ function intersect (rect, circle) {
   } else {
     targetPoint.x = circle.x
   }
-
-  if (circle.y > rect.y + rect.height) {
-    targetPoint.y = rect.y + rect.height
-  } else if (circle.y < rect.y) {
-    targetPoint.y = rect.y
-  } else {
-    targetPoint.y = circle.y
-  }
-
   return Math.sqrt(Math.pow(targetPoint.x - circle.x, 2) + Math.pow(targetPoint.y - circle.y, 2)) < circle.r
 }
 
